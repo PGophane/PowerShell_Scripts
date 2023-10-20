6e26562641b9eab0e3edbe23e09470609e5db75c
PGophane <pradnya.gophane@gmail.com>
2023-10-19 20:51:14 -0500

    Add files via upload

---
+++ DeleteSiteCollection.ps1
@@ -0,0 +1,10 @@
+﻿If ((Get-PSSnapIn -Name Microsoft.SharePoint.PowerShell -ErrorAction SilentlyContinue) -eq $null )
+{
+    Add-PSSnapIn -Name Microsoft.SharePoint.PowerShell
+}
+
+$site = Get-SPSite "<Site URL>"
+$siteId = $site.Id
+$siteDatabase = $site.ContentDatabase 
+
+Remove-SPContentDatabase $siteDatabase
