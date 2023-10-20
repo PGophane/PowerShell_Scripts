4f97aa16b40e2ff7b2fb524e6efeb67410dd13fc
PGophane <pradnya.gophane@gmail.com>
2023-10-19 20:13:37 -0500

    Add files via upload

---
+++ Apply_Standard_date_Format.ps1
@@ -0,0 +1,30 @@
+﻿If ((Get-PSSnapIn -Name Microsoft.SharePoint.PowerShell -ErrorAction SilentlyContinue) -eq $null )
+{
+    Add-PSSnapIn -Name Microsoft.SharePoint.PowerShell    
+}
+cls
+$sites = Get-SPSite "<Site URL>"
+$created = "Created"
+$modified = "Modified"
+
+#Write-Host $sites.AllWebs.Count
+
+  foreach ($web in $sites.AllWebs) 
+  {
+      Write-Host $web
+    $webLists = $web.Lists
+
+    for ($i = 0; $i -lt $webLists.Count; $i++)
+    {
+        $list = $web.Lists[$i];  
+        
+        $column = $list.Fields[$created]
+        $column.FriendlyDisplayFormat = "1"
+        $column.Update()
+
+        $column = $list.Fields[$modified]
+        $column.FriendlyDisplayFormat = "1"
+        $column.Update() 
+    }     
+  }
+ Write-Host "DONE...."
