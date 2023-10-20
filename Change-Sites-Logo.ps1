eb977b106d7c9635bf64e2af2ce5baf8f5709f7f
PGophane <pradnya.gophane@gmail.com>
2023-10-19 20:39:21 -0500

    Add files via upload

---
+++ Change-Sites-Logo.ps1
@@ -0,0 +1,20 @@
+﻿if ( (Get-PSSnapin -Name Microsoft.SharePoint.PowerShell -ErrorAction SilentlyContinue) -eq $null )
+ {
+     Add-PSSnapin Microsoft.SharePoint.PowerShell
+ }
+ cls
+ $sitelogo="/_layouts/15/Images/logo.png"
+
+$Site="<Site URL>"
+
+$Sites=new-object Microsoft.SharePoint.SPSite($Site)
+
+foreach($web in $Sites.Allwebs) 
+{
+    Write-Host $web.Title
+    $web.SiteLogoUrl=$sitelogo
+    $web.Update()
+}
+
+$Sites.Dispose()
+write-host "DONE !!!"
