3a800632e33bff49e4cbf6b079201b5af5ac4c7e
PGophane <pradnya.gophane@gmail.com>
2023-10-19 20:40:40 -0500

    Add files via upload

---
+++ delete_All_List_Items.ps1
@@ -0,0 +1,15 @@
+﻿If ((Get-PSSnapIn -Name Microsoft.SharePoint.PowerShell -ErrorAction SilentlyContinue) -eq $null )
+{
+    Add-PSSnapIn -Name Microsoft.SharePoint.PowerShell    
+}
+cls
+
+$webSite =  Get-SPWeb "<Site URL>"
+$spList = $webSite.Lists["<List Name>"] 
+
+foreach ($item in $spList.items)
+{
+    $deaditem=$splist.GetItemById($item.ID)
+    $deaditem.Delete()
+}
+write-host "DONE !!!"
