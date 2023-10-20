659fde303dd25bf6e57a41b5207547a6b7c01ec6
PGophane <pradnya.gophane@gmail.com>
2023-10-19 20:27:00 -0500

    Add files via upload

---
+++ inherit_Current_Navigation_From_Parent.ps1
@@ -0,0 +1,17 @@
+﻿if ( (Get-PSSnapin -Name Microsoft.SharePoint.PowerShell -ErrorAction SilentlyContinue) -eq $null )
+ {
+     Add-PSSnapin Microsoft.SharePoint.PowerShell
+ }
+ 
+ $sites = Get-SPSite "<Site URL>"
+
+foreach ($web in $sites.AllWebs) 
+  {
+    $web.AllowUnsafeUpdates = $true
+    $puWeb = [Microsoft.SharePoint.Publishing.PublishingWeb]:: GetPublishingWeb($web);
+    $puWeb.Navigation.InheritGlobal = $true
+    $puWeb.Navigation.InheritCurrent = $true
+    $puWeb.Update()    
+    $web.Update()    
+}
+write-host 'DONE !!!!'
